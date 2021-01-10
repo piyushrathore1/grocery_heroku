e92b0682d196254f983e5d1e0c572317cc59c361
Samundar <samundar@webnappmaker.in>
2021-01-10 08:52:11 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const conn = require("./config/db_config");
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+const app = express();
+app.use(bodyparser.json());
+/*app.get("/",(req,res)=>{
+    res.send("samundar");
+})*/
+
+const admin = require("./route/admin");
+app.use('/admin/',admin);
+
+const doctor = require("./route/doctor");
+app.use('/doctor/',doctor);
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.listen(3000,()=>{
+    console.log("server is connected");
+})
